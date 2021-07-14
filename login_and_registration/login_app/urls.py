82ff407f3faa7247083ffdeef6ea7d4c99453e37
jerome4914 <jeremyballew@hotmail.com>
2021-07-13 22:21:19 -0600

    completed registration and login

--- login_and_registration/login_app/urls.py
+++ login_and_registration/login_app/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.index),
     path('user/register', views.create_user),
     path('user/login', views.login_user),
-    path('user/logout', views.logout),
-    path('dashboard', views.dashboard),
+    path('success', views.success),
+    path('user/logout', views.logout)
 ]
--- login_and_registration/login_app/views.py
+++ login_and_registration/login_app/views.py
@@ -5,52 +5,56 @@ import bcrypt
 
 # Create your views here.
 def index(request):
+    request.session.flush()
     return render(request, "login_reg.html")
 
 def create_user(request):
-    errors = Registration.objects.basic_validator(request.POST)
-    user_list = Registration.objects.filter(email=request.POST['email'])
-    if user_list:
-        messages.error(request, "Email already exists")
-        return redirect('/')
+    if request.method == "POST":
+        errors = Registration.objects.basic_validator(request.POST)
 
-    if len(errors) > 0:
-        for key, value in errors.items():
-            messages.error(request, value, extra_tags=key)
-        return redirect('/')
-    print(request.POST['password'])
-    hashed_pw = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt()).decode()
-    print(hashed_pw)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value, extra_tags=key)
+            return redirect('/')
 
-    user1 = Registration.objects.create(
-        first_name = request.POST['first_name'],
-        last_name = request.POST['last_name'],
-        email = request.POST['email'],
-        password = hashed_pw,
-    )
-    request.session['log_user_id'] = user1.id
-    return redirect('/dashboard')
+        print(request.POST['password'])
+        hashed_pw = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt()).decode()
+        print(hashed_pw)
+
+        user1 = Registration.objects.create(
+            first_name = request.POST['first_name'],
+            last_name = request.POST['last_name'],
+            email = request.POST['email'],
+            password = hashed_pw,
+        )
+        request.session['user_id'] = user1.id
+        return redirect('/success')
+    return redirect('/')
+
+def success(request):
+    if 'user_id' not in request.session:
+        return redirect('/')
+    this_user = Registration.objects.filter(id=request.session['user_id'])
+    context = {
+        'user': this_user[0]
+    }
+    return render(request, 'dashboard.html', context)
 
 def login_user(request):
-    user_list = Registration.objects.filter(email=request.POST['email'])
-    if user_list:
-        logged_user = user_list[0]
-        if bcrypt.checkpw(request.POST['password'].encode(), logged_user.password.encode()):
-            request.session['log_user_id'] = logged_user.id
-            return redirect('/dashboard')
-        else:
-            messages.error(request, "Invalid email or password")
+    if request.method == "POST":
+        errors = Registration.objects.login_validator(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value, extra_tags=key)
             return redirect('/')
-    messages.error(request, "Email does not exist")
+        this_user = Registration.objects.filter(email=request.POST['email'])
+        request.session['user_id'] = this_user[0].id
+        return redirect('/success')
     return redirect('/')
 
 def logout(request):
     request.session.clear()
     return redirect('/')
 
-def dashboard(request):
-    context = {
-        'user': Registration.objects.get(id=request.session['log_user_id'])
-    }
-    return render(request, 'dashboard.html', context)
+
 
